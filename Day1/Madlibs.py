87b4103a866e172aa6005c492261aba9ec73fadf
Sarapdx <saraswathi.rajkumar1@gmail.com>
2024-05-23 16:05:58 -0700

    Madlibs
    add string variables and madlibs

---
+++ Day1/Madlibs.py
@@ -0,0 +1,7 @@
+print("Madlibs - Vacations")
+adj1 = input("Enter an Adjective\n")
+adj2 = input("Enter second Adjective\n")
+noun1 = input("Enter first noun\n")
+noun2 = input("Enter second noun\n")
+print("A vacation is when you take a trip to some " + adj1 +" place with your "+adj2 +
+      "family. Usually you go to some place that near a/an "+ noun1 + " or up on a/an "+noun2 +".")
---
+++ Day1/Stringvariables.py
@@ -0,0 +1,10 @@
+# output string in the console
+print("Hello World")
+# String with single quote 
+print("Go ahead, 'make my day'")
+# string with double quotes
+print('Success is the sum of "small efforts" - repeated day in and day out')
+# string with \n
+print("Strive not to be a success, \n but rather to be of value")
+# Concatenate
+print("hi" + "hello")
